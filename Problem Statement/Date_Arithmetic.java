8852fff2dd6e8ac2363e525f1578b93aad8c1a86
Satyam Gupta <Satyamgupta9012@gmail.com>
2025-06-19 09:19:52 +0530

    [Satyam Gupta] Add TimeZones and ZonedDateTime Completed

---
+++ Problem Statement/Date_Arithmetic.java
@@ -0,0 +1,20 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
+public class Date_Arithmetic {
+    public static void main(String [] args){
+        Scanner sc=new Scanner (System.in);
+        System.out.print("Enter date (yyyy-MM-dd):");
+        String input = sc.nextLine();
+
+        LocalDate date = LocalDate.parse(input);
+        LocalDate result = date
+                .plusDays(7)
+                .plusMonths(1)
+                .plusYears(2)
+                .minusWeeks(3);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        System.out.println("Final Date: " + result.format(formatter));
+
+    }
+}
